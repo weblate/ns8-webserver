3b3c456dda834eb5aae039a9339022814fec72b7
Stephane de Labrusse <stephdl@de-labrusse.fr>
2022-03-17 19:05:15 +0100

    Modify name newui to webserver

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    appName: "newui", // TODO
+    appName: "webserver", // TODO
     instanceName: "",
     instanceLabel: "",
     core: null,
